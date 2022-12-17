4e42a185ed8957b41c013e69ebf571e31771a940
brandonszeto <brandonszeto@gmail.com>
2022-12-16 22:27:37 -0800

    Default settings

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,44 +1,35 @@
-local telescope = require("telescope")
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+	return
+end
+
 telescope.setup({
 	defaults = {
-		scroll_strategy = "cycle",
-		vimgrep_arguments = {
-			"rg",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-			"--hidden",
-			-- "-g '!.git'",
+		-- Default configuration for telescope goes here:
+		-- config_key = value,
+		mappings = {
+			i = {
+				-- map actions.which_key to <C-h> (default: <C-/>)
+				-- actions.which_key shows the mappings for your picker,
+				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+				["<C-h>"] = "which_key",
+			},
 		},
 	},
-
 	pickers = {
-		find_files = {
-			theme = "dropdown",
-			-- uses .ignore file in home dir
-			find_command = { "rg", "--files", "--hidden" },
-		},
-		git_files = {
-			theme = "dropdown",
-		},
-		live_grep = {
-			theme = "dropdown",
-		},
+		-- Default configuration for builtin pickers goes here:
+		-- picker_name = {
+		--   picker_config_key = value,
+		--   ...
+		-- }
+		-- Now the picker_config_key will be applied every time you call this
+		-- builtin picker
 	},
-
 	extensions = {
-		fzf = {
-			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = false, -- override the generic sorter
-			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
-		},
+		-- Your extension configuration goes here:
+		-- extension_name = {
+		--   extension_config_key = value,
+		-- }
+		-- please take a look at the readme of the extension you want to configure
 	},
 })
-
-telescope.load_extension("fzf")
--- telescope.load_extension("neoclip")
